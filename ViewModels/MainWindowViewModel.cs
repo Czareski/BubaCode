5e333793fbe33441569f2143535b2d2159163d04
Czareski <kubaucher202@gmail.com>
2025-12-05 02:01:36 +0100

    Custom control, editing text, caret

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace BubaCode.ViewModels;
+﻿using System.Diagnostics;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BubaCode.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string CodeSample { get; } = "public partial class MainWindowViewModel : ViewModelBase \r\n{ \r\n\t public int example = -1; \r\n} \r\n";
+    public int GetLinesCount()
+    {
+        return CodeSample.Split("\r\n").Length;
+    }
+    [RelayCommand]
+    public void Save()
+    {
+        Debug.WriteLine("Save");
+    }
 }
